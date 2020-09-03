cea0ef27023b7c825f9f7d0ef4fdecf4c614bff6
Aaron Meese <ajmeese7@gmail.com>
2020-09-03 09:27:50 -0500

    Moved fetch requirement
    I neglected to move that with the functions that use the node-fetch library.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fetch = require('node-fetch');
 const path = require('path');
 const PORT = process.env.PORT || 5000; // Heroku or local
 const menu = require('./menu');
--- transcript.js
+++ transcript.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch');
 require('dotenv').config();
 
 async function createTranscript(recordingUrl, fileName) {
